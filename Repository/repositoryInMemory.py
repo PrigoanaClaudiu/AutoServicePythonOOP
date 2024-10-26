43695aca794345857f21e469a9fc92783ce23bd4
PrigoanaClaudiu <claudiu_prigoana16@yahoo.com>
2024-10-26 13:29:15 +0300

    repository in memory

--- Repository/repositoryInMemory.py
+++ Repository/repositoryInMemory.py
@@ -7,13 +7,25 @@ class RepositoryInMemory(Repository):
         self.entities = {}
 
     def read(self, idEntity=None):
-        pass
+        if idEntity is None:
+            return list(self.entities.values())
+        if idEntity in self.entities:
+            return self.entities[idEntity]
+        else:
+            return None
 
     def add(self, entity: Entity):
-        pass
+        if self.read(entity.idEntity) is not None:
+            raise KeyError("Entity already exists")
+        self.entities[entity.idEntity] = entity
 
     def remove(self, idEntity):
-        pass
+        if self.read(idEntity) is None:
+            raise KeyError("Entity not found")
+        del self.entities[idEntity]
 
     def update(self, entity: Entity):
-        pass
+        if self.read(entity.idEntity) is None:
+            raise KeyError("Entity not found")
+
+        self.entities[entity.idEntity].update(entity)
