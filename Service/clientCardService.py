1cca9b9bacae16eec5ff8aa9aa7ca263f058fa9f
PrigoanaClaudiu <claudiu_prigoana16@yahoo.com>
2024-10-26 16:18:14 +0300

    clientCard crud service

---
+++ Service/clientCardService.py
@@ -0,0 +1,29 @@
+from Entities.clientCard import ClientCard
+from Repository.repository import Repository
+from Entities.clientCardValidation import ClientValidator
+
+
+class ClientCardService:
+    def __init__(self, repositoryCard: Repository, clientVali: ClientValidator):
+        self.__clientValidator = clientVali
+        self.__repository = repositoryCard
+
+    def getClientCard(self):
+        self.__repository.read()
+
+    def addClientCard(self, idClient, name, surname, CNP, date_of_birth, date_of_registration):
+        card = ClientCard(idClient, name, surname, CNP, date_of_birth, date_of_registration)
+
+        self.__clientValidator.valideaza(card)
+        self.__repository.add(card)
+
+    def removeClientCard(self, idClient):
+        client = self.__repository.read(idClient)
+
+        self.__repository.remove(client)
+
+    def updateClientCard(self, idClient, name, surname, CNP, date_of_birth):
+        client = ClientCard(idClient, name, surname, CNP, date_of_birth)
+
+        self.__clientValidator.valideaza(client)
+        self.__repository.update(client)
